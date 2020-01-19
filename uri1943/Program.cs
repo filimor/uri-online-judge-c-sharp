7b7c7896db4f0fb94469a5221a34ee640b5b4462
Filipe Moreira <filimor@posteo.com.br>
2020-01-19 04:06:25 -0300

    2 recomendacoes (1943 e 2748)

---
+++ uri1943/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace uri1943
+{
+    internal static class TopN
+    {
+        private static void Main()
+        {
+            int.TryParse(Console.ReadLine(), out int k);
+            if (k == 1)
+            {
+                Console.WriteLine("Top 1");
+            }
+            else if (k <= 3)
+            {
+                Console.WriteLine("Top 3");
+            }
+            else if (k <= 5)
+            {
+                Console.WriteLine("Top 5");
+            }
+            else if (k <= 10)
+            {
+                Console.WriteLine("Top 10");
+            }
+            else if (k <= 25)
+            {
+                Console.WriteLine("Top 25");
+            }
+            else if (k <= 50)
+            {
+                Console.WriteLine("Top 50");
+            }
+            else
+            {
+                Console.WriteLine("Top 100");
+            }
+        }
+    }
+}
---
+++ uri2748/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uri2748
+{
+    internal static class Saida2
+    {
+        private static void Main()
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("|        Roberto                      |");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("|        5786                         |");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("|        UNIFEI                       |");
+            Console.WriteLine("---------------------------------------");
+        }
+    }
+}
