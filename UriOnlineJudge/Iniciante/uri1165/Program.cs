3849cfc50f1b7b39dff96e77b3df3cc27fd567a3
Filipe Moreira <filimor@posteo.com.br>
2020-01-30 12:44:50 -0300

    Test commit

---
+++ UriOnlineJudge/Iniciante/uri1165/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace uri1165 // Número Primo
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            int.TryParse(Console.ReadLine(), out int n);
+            bool primo;
+
+            for (int i = 1; i <= n; i++)
+            {
+                primo = true;
+                int.TryParse(Console.ReadLine(), out int x);
+                if (x == 1)
+                {
+                    primo = false;
+                }
+                else
+                {
+                    for (int j = 2; j < x; j++)
+                    {
+                        if (x % j == 0)
+                        {
+                            primo = false;
+                        }
+                    }
+                }
+                Console.WriteLine($"{x} {(primo ? "eh" : "nao eh")} primo");
+            }
+        }
+    }
+}
